c420cd9cef2e357bc194ddc3be97439800d7a3a6
garrycnx <63348994+garrycnx@users.noreply.github.com>
2024-01-27 16:41:41 +0530

    done

--- script.js
+++ script.js
@@ -3,18 +3,41 @@ const close = document.getElementById('close');
 const nav = document.getElementById('navbar')
 
 if (bar) {
-       bar.addEventListener('click', () =>{
-       nav.classList.add('active'); 
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
 
-    }) 
+    })
 
 }
 
 if (close) {
-    close.addEventListener('click', () =>{
-    nav.classList.remove('active'); 
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
 
- }) 
+    })
 
 }
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+        var loginModal = document.getElementById("loginModal");
+        var closeModal = document.getElementById("closeModal");
+
+        document.querySelector("a[href='#']").addEventListener("click", function () {
+            loginModal.style.display = "block";
+        });
+
+        closeModal.addEventListener("click", function () {
+            loginModal.style.display = "none";
+        });
+
+        window.addEventListener("click", function (event) {
+            if (event.target === loginModal) {
+                loginModal.style.display = "none";
+            }
+        });
+    });
 
